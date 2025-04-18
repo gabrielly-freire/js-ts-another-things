653378e109d18662a9127c283878140479a99e3d
Gabrielly Freire <gabylopesfreire@gmail.com>
2025-04-18 18:31:38 -0300

    feat: adição de css module

--- react/01-fundamentos-reactjs/src/App.jsx
+++ react/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import "./styles.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="Gabrielly Freire" content="This is my first post!" />
       <Post author="Gabrielly Freire" content="This is my second post!" />
       <Post author="Gabrielly Freire" content="This is my third post!" />
