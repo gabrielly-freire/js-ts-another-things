7687c3e78338f826d4d45351aad6da1e3f96381b
Gabrielly Freire <gabylopesfreire@gmail.com>
2025-04-18 19:12:15 -0300

    feat: adição de componente sidebar

--- react/01-fundamentos-reactjs/src/App.jsx
+++ react/01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,22 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Gabrielly Freire" content="This is my first post!" />
-      <Post author="Gabrielly Freire" content="This is my second post!" />
-      <Post author="Gabrielly Freire" content="This is my third post!" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Gabrielly Freire" content="This is my first post!" />
+          <Post author="Gabrielly Freire" content="This is my second post!" />
+          <Post author="Gabrielly Freire" content="This is my third post!" />
+        </main>
+      </div>
     </div>
   );
 }
--- react/01-fundamentos-reactjs/src/components/Header.jsx
+++ react/01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import styles from "./Header.module.css";
+import logo from "../assets/ignite-logo.png";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={logo} alt="Logo ignite" />
     </header>
   );
 }
