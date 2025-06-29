314ed15497f27002a8b290d92f5c0b3512b90067
Gabrielly Freire <gabylopesfreire@gmail.com>
2025-06-29 20:03:44 -0300

    feat: finalizando o histórico

--- react/02-ignite-time/src/pages/History/index.tsx
+++ react/02-ignite-time/src/pages/History/index.tsx
@@ -1,3 +1,47 @@
+import { HistoryContrainer, HistoryListContrainer, Status } from "./style";
+
 export function History(){
-    return <h1>History</h1>
+    return (
+        <HistoryContrainer>
+            <h1>Meu histórico</h1>
+           
+           <HistoryListContrainer>
+                <table>
+                  <thead>
+                    <th>Tarefa</th>
+                    <th>Duração</th>
+                    <th>Início</th>
+                    <th>Status</th>
+                  </thead> 
+
+                  <tbody>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>há 2 mese</td>
+                        <td><Status statusColor='green'>Concluido</Status></td>
+                    </tr>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>há 2 mese</td>
+                        <td><Status statusColor='green'>Concluido</Status></td>
+                    </tr>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>há 2 mese</td>
+                        <td><Status statusColor='yellow'>Em andamento</Status></td>
+                    </tr>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>há 2 mese</td>
+                        <td><Status statusColor='red'>Interrompido</Status></td>
+                    </tr>
+                  </tbody> 
+                </table>
+           </HistoryListContrainer>
+        </HistoryContrainer>
+    )
 }
